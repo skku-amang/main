0489d40c1cc12df214cccfeafc5dd8dc251c2b2b
GalaxyBook2 <manamana32321@gmail.com>
2024-08-21 07:35:01 +0900

    chore: add TODOs

--- src/app/(etc)/teams/page.tsx
+++ src/app/(etc)/teams/page.tsx
@@ -13,6 +13,14 @@ const rows: TeamColumn[] = TEAMS.map((team) => ({
   is_freshmanFixed: team.is_freshmanFixed
 }))
 
+// TODO: column visible 선택 기능 -> 세션별 지원자 확인 할 수 있게
+// TODO: 검색 기준을 곡명이 아니라 모든 것으로 확장
+// TODO: column 너비 조절
+// TODO: 필터 -> Dialog로 처리
+// TODO: Footer 맨 밑으로 내리기
+// TODO: Pagination에서 1,2,3,4,5 등 추가
+// TODO: DataTable Header 색상 변경
+// TODO: Primary, Secondary 색상 설정
 const TeamList = () => {
   return (
     <div className="container">
