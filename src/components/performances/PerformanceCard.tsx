999c2d0869b6f2f9b39db499ef39b02235512839
GalaxyBook2 <manamana32321@gmail.com>
2024-08-22 23:39:11 +0900

    feat: add PerformanceCreate page

--- src/components/performances/PerformanceCard.tsx
+++ src/components/performances/PerformanceCard.tsx
@@ -2,6 +2,7 @@
 
 import {
   Card,
+  CardContent,
   CardDescription,
   CardFooter,
   CardHeader,
@@ -11,30 +12,49 @@ import { IoLocationSharp } from "react-icons/io5";
 import Image from "next/image"
 import Link from "next/link"
 import ROUTES from "../../../constants/routes"
+import { cn } from "@/lib/utils";
 
 interface PerformanceCardProp {
-  id: number
+  id?: number
   name: string
   representativeSrc: string
+  description?: string
   location: string
   startDatetime: Date
   endDatetime: Date
+  className?: string
 }
 
-const PerformanceCard = ({ id, name, representativeSrc, location, startDatetime, endDatetime }: PerformanceCardProp) => {
+const RepresentativeImage = ({ alt, src, width, height }: { alt: string, src: string, width: number, height: number}) => {
+  return <Image alt={alt} src={src} width={width} height={height} className="overflow-hidden" />
+}
+
+const PerformanceCard = ({ id, name, representativeSrc, description, location, startDatetime, endDatetime, className }: PerformanceCardProp) => {
   const width = 300
+  const height = 200
+
   return (
-    <Card style={{ width }} className="overflow-hidden">
-      <Link href={ROUTES.PERFORMANCE.DETAIL.index.url(id.toString())}>
-        <Image alt={`${name} 이미지`} src={representativeSrc} width={width} height={200} />
-      </Link>
+    <Card style={{ width }} className={cn("overflow-hidden", className)}>
+      {id ?
+        <Link href={ROUTES.PERFORMANCE.DETAIL.index.url(id.toString())}>
+          <RepresentativeImage alt={`${name} 이미지`} src={representativeSrc} width={width} height={height} />
+        </Link>
+        :
+        <RepresentativeImage alt={`${name} 이미지`} src={representativeSrc} width={width} height={height} />
+      }
 
       <CardHeader className="pb-0">
         <CardTitle>{name}</CardTitle>
         <CardDescription>
-          {`${startDatetime.getFullYear()}년 ${startDatetime.getMonth()}월 ${startDatetime.getDate()}일`}<br/>
+          {`${startDatetime.getFullYear()}년 ${startDatetime.getMonth()}월 ${startDatetime.getDate()}일`}
         </CardDescription>
       </CardHeader>
+
+      {description &&
+        <CardContent className="truncate">
+          {description}
+        </CardContent>
+      }
 
       <CardFooter className="flex text-slate-400 text-sm">
         <IoLocationSharp />&nbsp;
