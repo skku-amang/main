e9a49d9c13a02ae1f68a95cdd8df1adec6b1823f
GalaxyBook2 <manamana32321@gmail.com>
2024-08-25 20:17:50 +0900

    feat: add Imageloader and YoutubeVideo util

---
+++ src/components/common/ImageLoader.tsx
@@ -0,0 +1,38 @@
+'use client'
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { cn } from '@/lib/utils';
+
+function ImageLoader({ src, alt, className }: { src: string, alt: string, className?: string }) {
+  const [isImageLoaded, setIsImageLoaded] = useState(false);
+
+  const handleImageLoad = () => {
+    setIsImageLoaded(true);
+  };
+
+  return (
+    <div className={cn(className, 'relative w-full h-full')}>
+      <Image
+        src={src}
+        alt={alt}
+        fill
+        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        style={{
+          objectFit: 'cover',
+          opacity: isImageLoaded ? 1 : 0.8,
+          transition: 'opacity 1s ease-in-out',
+        }}
+        onLoad={handleImageLoad}
+      />
+      {!isImageLoaded && (
+        <div
+          className='flex items-center justify-center absolute top-0 left-0 w-full h-full bg-gray-500'>
+          <p>Loading...</p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ImageLoader;
---
+++ src/lib/youtube/Player.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+interface YoutubePlayerProps extends React.IframeHTMLAttributes<HTMLIFrameElement> {
+  videoId: string;
+}
+
+const YoutubePlayer: React.FC<YoutubePlayerProps> = ({ videoId, ...iframeProps }) => {
+  return (
+    <iframe
+      src={`https://www.youtube.com/embed/${videoId}`}
+      {...iframeProps}
+    />
+  );
+};
+
+export default YoutubePlayer;
