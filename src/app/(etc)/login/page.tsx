f529d47a86380ae25d1169e1f49f833fee6ed8fc
GalaxyBook2 <manamana32321@gmail.com>
2024-08-26 00:08:27 +0900

    chore: minor changes

--- src/app/(etc)/login/page.tsx
+++ src/app/(etc)/login/page.tsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import styles from './login.module.css'; // CSS 모듈을 import합니다.
+
 import { Input } from "@/components/ui/input";
+
+import styles from './login.module.css';
 
 const Login = () => {
   return (
     <div className="flex justify-center">
       <div className="h-[698px] w-[1152px] flex justify-center items-center gap-[100px] bg-white rounded-[15px] mt-[50px] mb-[60px]">
         <div className={`w-[490px] h-[644px] rounded-[50px] ${styles.gradation}`}></div> 
-        <div className="flex flex-col justify-center items-center mb-[45px] mr-[100px] mr-[50px] ml-[30px]">           
+        <div className="flex flex-col justify-center items-center mb-[45px] mr-[100px] ml-[30px]">           
           <h3 className="text-[35px] font-[600] mb-[30px]">Login</h3>
           <div className="flex flex-col gap-[30px]">
             <div><Input name="id" placeholder="ID" className="rounded-[50px] w-[330px] h-[50px]"/></div>
