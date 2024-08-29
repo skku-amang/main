39d3fc3a2ab1300f15721907d7993c65a5fbe4f1
GalaxyBook2 <manamana32321@gmail.com>
2024-08-29 10:37:17 +0900

    hotfix: replace faker locale

--- lib/dummy/User.ts
+++ lib/dummy/User.ts
@@ -1,18 +1,31 @@
-import { getRandomSessions } from "@/lib/dummy/Session";
+import { faker } from '@faker-js/faker'
 
-import { User } from "../../types/User";
-import { customFaker } from ".";
-import dummyGenerations from "./Generation";
+import { getRandomSessions } from '@/lib/dummy/Session'
 
-export const createUser = (id: number): User => ({
-  id,
-  name: customFaker.person.fullName(),
-  nickname: customFaker.internet.userName(),
-  email: customFaker.internet.email(),
-  bio: customFaker.lorem.sentence(),
-  profile_image: customFaker.image.avatar(),
-  generation: customFaker.helpers.arrayElement(dummyGenerations),
-  sessions: getRandomSessions(new Map([[1, 0.7], [2, 0.2], [3, 0.1]])),
-  genre: customFaker.music.genre(),
-  liked_artists: customFaker.person.firstName()
-})
+import { User } from '../../types/User'
+import { customFaker } from '.'
+import dummyGenerations from './Generation'
+
+export const createUser = (id: number): User => {
+  let fakerWithSeed = faker
+  fakerWithSeed.seed(customFaker.seed())
+
+  return {
+    id,
+    name: customFaker.person.fullName(),
+    nickname: customFaker.internet.userName(),
+    email: customFaker.internet.email(),
+    bio: customFaker.lorem.sentence(),
+    profile_image: customFaker.image.avatar(),
+    generation: customFaker.helpers.arrayElement(dummyGenerations),
+    sessions: getRandomSessions(
+      new Map([
+        [1, 0.7],
+        [2, 0.2],
+        [3, 0.1]
+      ])
+    ),
+    genre: fakerWithSeed.music.genre(),
+    liked_artists: customFaker.person.firstName()
+  }
+}
