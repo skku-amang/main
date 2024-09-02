d1f64f118896405724bad0191ff614f32ea36081
youngjju git <2000dudwn@naver.com>
2024-09-02 18:24:03 +0900

    chore: mobile version navigation bar making in progress

--- components/common/Header/index.tsx
+++ components/common/Header/index.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -5,6 +6,7 @@ import ROUTES from '../../../constants/routes'
 import { cn } from '../../../lib/utils'
 import NavLink from '../NavLink'
 import Profile from './Profile'
+import { useState } from 'react'
 
 const Header = ({
   position,
@@ -20,6 +22,12 @@ const Header = ({
     { name: '맴버목록', url: ROUTES.MEMBER.LIST.url, active: true }
   ]
 
+  const [isOpen, setIsOpen] = useState(false);
+
+  const OpenSideMenu = () => {
+    setIsOpen(!isOpen);
+  }
+
   return (
     <header
       className={cn(
@@ -28,12 +36,14 @@ const Header = ({
       )}
       style={{ height }} >
 
-      {/* Tablet & Desktop */}
+      {/* Mobile */}
       <nav className="flex justify-between w-full items-center md:hidden">
-        <div className='flex flex-col gap-[0.3rem] ml-6'>
-            <div className='bg-white w-[30px] h-[0.2rem]'></div>
-            <div className='bg-white w-[30px] h-[0.2rem]'></div>
-            <div className='bg-white w-[30px] h-[0.2rem]'></div>
+        <div className='flex-col h-full items-center justify-center pl-4' onClick={OpenSideMenu}>
+          <div className='flex flex-col justify-center h-full gap-[0.3rem]'>
+            <div className='bg-white w-8 h-[0.2rem]'></div>
+            <div className='bg-white w-8 h-[0.2rem]'></div>
+            <div className='bg-white w-8 h-[0.2rem]'></div>
+          </div>
         </div>
         <div className='flex items-center'>
           <Link href="/">
@@ -44,6 +54,12 @@ const Header = ({
         </div>
       </nav>
 
+      {/* 모바일 열리는 창 전체 */}
+      <div className={`flex flex-col fixed top-0 left-0 h-screen w-[45%] bg-gray-800 text-white transform ${isOpen ? 'translate-x-0' : '-translate-x-full'} transition-transform duration-300 ease-in-out`}>
+        <div className='flex-1' onClick={OpenSideMenu}>1번</div>
+        <div className='flex-1'>2번</div>
+        <div className='flex-1'>3번</div>
+      </div>
 
       {/* Tablet & Desktop */}
       <nav className="hidden md:flex h-full w-full items-center px-10 md:visible">
--- components/common/NavLink.tsx
+++ components/common/NavLink.tsx
@@ -7,10 +7,10 @@ import React from 'react'
 import { cn } from '../../lib/utils'
 
 const NavLink = ({ href, children, active }: { href: string, children: React.ReactNode, active: boolean }) => {
-  const pathname = usePathname()
-  const parentPathname = pathname.split('/')[1]
-  const hrefWithoutSlash = href.replaceAll("/", "")
-  const activeClass = parentPathname === hrefWithoutSlash ? 'text-white' : 'text-gray-400'
+  const pathname = usePathname() // 주소 가져오기
+  const parentPathname = pathname.split('/')[1] // 주소를 가져와서 어디 페이지인지 판별
+  const hrefWithoutSlash = href.replaceAll("/", "")  // 주소에서 /을 모두 제거
+  const activeClass = parentPathname === hrefWithoutSlash ? 'text-white' : 'text-gray-400' // 현재 있는 페이지의 버튼만 흰색
   const hoverClass = "hover:border-b-[0.2rem] hover:border-t-[0.2rem] hover:border-t-primary hover:border-b-white hover:text-white"
 
   return (
