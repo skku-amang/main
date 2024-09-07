0de4d265ccec3c6c4bc7a4cf99ddce8968bf06b3
GalaxyBook2 <manamana32321@gmail.com>
2024-09-08 03:23:20 +0900

    chore: polish scrollbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,12 @@ export const metadata: Metadata = {
   }
 }
 
+/**
+ * Root layout 입니다.
+ * Session, metadata, font 등 전역적인 설정을 적용합니다.
+ * HTML, CSS 등 스타일 절대 적용 금지!
+ * 개별 페이지의 레이아웃은 각 페이지에서 구현해주세요.
+ */
 export default function RootLayout({
   children
 }: {
