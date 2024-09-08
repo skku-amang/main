08fbf59d1ba18379ef615bde33901bfd289e6933
youngjju git <2000dudwn@naver.com>
2024-09-08 23:22:10 +0900

    chore: Create teams page 1st page not complete

--- app/(general)/teams/create/page.tsx
+++ app/(general)/teams/create/page.tsx
@@ -10,6 +10,15 @@ import {
   SelectTrigger,
   SelectValue,
 } from "@/components/ui/select"
+import {
+  Pagination,
+  PaginationContent,
+  PaginationEllipsis,
+  PaginationItem,
+  PaginationLink,
+  PaginationNext,
+  PaginationPrevious,
+} from "@/components/ui/pagination"
 
 
 const TeamCreate = () => {
@@ -61,7 +70,177 @@ const TeamCreate = () => {
               </div>
             </div>
           </div>
+          
+          <div className="flex flex-col bottom-0 justify-center items-center sm:absolute sm:w-full h-[55%] lg:h-[57%]">
+            <div className="w-full h-full flex flex-col pl-[4.8rem] pt-[4rem]">
+              <div className="flex flex-col items-center w-full sm:items-start sm:left-20 sm:top-20 pb-10">
+                <div className="font-semibold text-lg">세션 정보</div>
+                <div className="font-medium text-base">곡의 필요한 모든 세션을 체크해주세요</div>
+              </div>
+              <div className="w-full flex gap-20 pb-5">
+                <div className="flex items-center space-x-2">
+                  <h5 className="font-medium pr-[13rem]">보컬</h5>
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    보컬1
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    보컬1
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    보컬1
+                  </label>
+                </div>
+              </div>
+              <div className="w-full flex gap-20 pb-5">
+                
+                
+                <div className="flex items-center space-x-2">
+                  <h5 className="font-medium pr-[13rem] ">기타</h5>
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    기타1
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    기타2
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    기타3
+                  </label>
+                </div>
+              </div>
+              <div className="w-full flex gap-[4.1rem] pb-5">
+                
+                
+                <div className="flex items-center space-x-2">
+                  <h5 className="font-medium pr-[8.5rem]">베이스 및 드럼</h5>
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    베이스1
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    베이스2
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    드럼
+                  </label>
+                </div>
+              </div>
 
+              <div className="w-full flex gap-20 pb-5">
+                
+                
+                <div className="flex items-center space-x-2">
+                  <h5 className="font-medium pr-[13rem]">신디</h5>
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    신디1
+                  </label>
+                </div>
+                <div className="flex items-center space-x-2">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    신디2
+                  </label>
+                </div>
+              </div>
+              <div className="w-full flex gap-[4.5rem] pb-5">
+                <div className="flex items-center space-x-2">
+                  <h5 className="font-medium pr-[12.8rem]">그 외</h5>
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    현악기
+                  </label>
+                </div>
+                <Input className="w-24 pl-52" type="email" id="songName" placeholder="악기명" />
+              </div>
+              <div className="flex items-center pl-[15.5rem]">
+                  <Checkbox id="terms" />
+                  <label
+                    htmlFor="terms"
+                    className="text-sm pl-2 pr-[4.5rem] font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                  >
+                    관악기
+                  </label>
+                  <Input className="w-24 pl-52" type="email" id="songName" placeholder="악기명"/>
+              </div>
+              
+            </div>
+              <Pagination className="pb-9">
+                <PaginationContent>
+                  <PaginationItem>
+                    <PaginationPrevious href="#" />
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#" isActive>
+                      1
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationLink href="#">
+                      2
+                    </PaginationLink>
+                  </PaginationItem>
+                  <PaginationItem>
+                    <PaginationNext href="#" />
+                  </PaginationItem>
+                </PaginationContent>
+              </Pagination>
+          </div>
         </div>
      </div>
     </>
