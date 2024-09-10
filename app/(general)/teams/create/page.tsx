b78f9e9214d365a825cc1c9afaf151f7b90a32ff
youngjju git <2000dudwn@naver.com>
2024-09-11 01:12:40 +0900

    chore: page change done 2nd page let's go

--- app/(general)/teams/create/page.tsx
+++ app/(general)/teams/create/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import { Checkbox } from "@/components/ui/checkbox"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
@@ -19,12 +20,17 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination"
+import { useState } from "react"
+
+
 
 
 const TeamCreate = () => {
+
+  const [currentPage, setCurrentPage] = useState(1);
+
   return (
     <>
-
       <div className="relative flex flex-col items-center w-full h-auto overflow-x-hidden">
         <div className="fixed w-screen h-64 bg-primary">
         </div>
@@ -32,7 +38,9 @@ const TeamCreate = () => {
           <h1 className="font-medium text-3xl sm:text-5xl text-white pb-6">Create New Team</h1>
           <div className="w-[15rem] h-[0.1rem] sm:w-[25rem] sm:h-[0.15rem] bg-white"></div>
         </div>
-        <div className="relative w-4/5 h-[80rem] lg:h-[70rem] shadow-xl rounded-lg mt-10 mb-12 bg-gray-50 z-10">
+        <div className={`${currentPage === 2 ? "h-0 hidden" : ""}
+         relative w-4/5 h-[80rem] lg:h-[70rem] shadow-xl rounded-lg mt-10 mb-12 bg-gray-50 z-10}`}
+         >
           <div className="flex flex-col justify-center items-center gap-11 sm:w-full h-[45%] lg:h-[43%] border-b-zinc-200 border-b-[1px]">
             <div className="flex flex-col items-center sm:items-start sm:absolute sm:left-20 sm:top-20">
               <div className="font-semibold text-lg">공연 및 곡 정보</div>
@@ -173,8 +181,6 @@ const TeamCreate = () => {
               </div>
 
               <div className="w-full flex gap-20 pb-5">
-                
-                
                 <div className="flex items-center space-x-2">
                   <h5 className="font-medium pr-[13rem]">신디</h5>
                   <Checkbox id="terms" />
@@ -219,29 +225,63 @@ const TeamCreate = () => {
                   <Input className="w-24 pl-52" type="email" id="songName" placeholder="악기명"/>
               </div>
               
-            </div>
+              </div>
               <Pagination className="pb-9">
                 <PaginationContent>
                   <PaginationItem>
-                    <PaginationPrevious href="#" />
+                    <PaginationPrevious/>
                   </PaginationItem>
                   <PaginationItem>
                     <PaginationLink href="#" isActive>
                       1
                     </PaginationLink>
                   </PaginationItem>
-                  <PaginationItem>
+                  <PaginationItem onClick={() => setCurrentPage(2)}>
                     <PaginationLink href="#">
                       2
                     </PaginationLink>
-                  </PaginationItem>
-                  <PaginationItem>
-                    <PaginationNext href="#" />
+                  </PaginationItem >
+                  <PaginationItem onClick={() => setCurrentPage(2)}>
+                    <PaginationNext href="#"/>
                   </PaginationItem>
                 </PaginationContent>
               </Pagination>
+            </div>
           </div>
-        </div>
+
+        <div className={`${currentPage=== 1 ? "h-0 hidden" : ""}
+         flex flex-col items-center relative w-4/5 h-[80rem] lg:h-[70rem] shadow-xl rounded-lg mt-10 mb-12 bg-gray-50 z-10}`}
+         >
+            <div className="w-full h-[43%] border-b-[1px] border-b-zinc-200"></div>
+            <div className="flex flex-col items-center w-full h-[57%]">
+              <div className="w-[90%] h-[89%] bg-gray-100">
+              </div>
+              <div>
+                  <Pagination className="pb-9">
+                    <PaginationContent>
+                      <PaginationItem onClick={() => setCurrentPage(1)}>
+                        <PaginationPrevious href="#"/>
+                      </PaginationItem>
+                      <PaginationItem onClick={() => setCurrentPage(1)}>
+                        <PaginationLink href="#">
+                          1
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationLink href="#">
+                          2
+                        </PaginationLink>
+                      </PaginationItem>
+                      <PaginationItem>
+                        <PaginationNext href=""/>
+                      </PaginationItem>
+                    </PaginationContent>
+                  </Pagination>
+                </div>
+            </div>
+
+         </div>
+         
      </div>
     </>
 
