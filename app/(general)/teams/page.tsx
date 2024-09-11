dec8e30712274c6b56c2d338626ded53f9470075
GalaxyBook2 <manamana32321@gmail.com>
2024-09-11 15:01:03 +0900

    chore: seperate RelatedPerformanceList from TeamList

--- app/(general)/teams/page.tsx
+++ app/(general)/teams/page.tsx
@@ -1,15 +1,11 @@
-import Link from "next/link"
-
+import RelatedPerformanceList from "@/app/(general)/teams/[id]/_components/RelatedPerformanceList"
 import API_ENDPOINTS from "@/constants/apiEndpoints"
+import { generateDummys } from "@/lib/dummy"
+import { createTeam } from "@/lib/dummy/Team"
 import fetchData from "@/lib/fetch"
 import { ListResponse } from "@/lib/fetch/responseBodyInterfaces"
 import { Team } from "@/types/Team"
 
-import { Badge } from "../../../components/ui/badge"
-import ROUTES from "../../../constants/routes"
-import { generateDummys } from "../../../lib/dummy"
-import { createPerformance } from "../../../lib/dummy/Performance"
-import { createTeam } from "../../../lib/dummy/Team"
 import { columns } from "./_components/TeamListTable/columns"
 import { TeamListDataTable } from "./_components/TeamListTable/data-table"
 
@@ -27,7 +23,6 @@ const _TEAMS = generateDummys(45, createTeam)
 // TODO: 검색 기준을 곡명이 아니라 모든 것으로 확장
 // TODO: Pagination에서 1,2,3,4,5 등 추가
 const TeamList = async () => {
-  const activePerformances = generateDummys(3, createPerformance)
   const res = await fetchData(API_ENDPOINTS.TEAM.LIST, {
     cache: "no-cache"
   })
@@ -49,19 +44,9 @@ const TeamList = async () => {
         <h2 className="mt-24 text-4xl font-extrabold text-gray-600">
           공연팀 목록
         </h2>
-        <p className="my-8 font-bold">Performances</p>
-        <div className="flex gap-x-4">
-          {activePerformances.map((p) => (
-            <Link
-              key={p.id}
-              href={ROUTES.PERFORMANCE.DETAIL.url(p.id.toString())}
-            >
-              <Badge className="text-md rounded-xl bg-slate-200 px-6 py-1 font-normal text-black">
-                {p.name}
-              </Badge>
-            </Link>
-          ))}
-        </div>
+
+        {/* 연관된 공연 목록 */}
+        <RelatedPerformanceList />
       </div>
 
       {/* 팀 목록 테이블 */}
