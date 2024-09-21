2eac8ffc7ddbdb0ef932a8c2e2d312a2aab651a5
youngjju git <2000dudwn@naver.com>
2024-09-21 19:26:52 +0900

    chore: login page done?!

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -11,6 +11,13 @@ import { Input } from "@/components/ui/input"
 import { emailSchema, passwordSchema } from "@/constants/zodSchema"
 import { Label } from "@/components/ui/label"
 
+import { Poppins } from "next/font/google"
+import { cn } from "@/lib/utils"
+import Link from "next/link"
+import ROUTES from "@/constants/routes"
+
+const Poppin = Poppins({ subsets: ['latin'], weight: '400' })
+
 const formSchema = z.object({
   email: emailSchema,
   password: passwordSchema
@@ -28,45 +35,59 @@ const Login = () => {
   }
 
   return (
-    <div className="flex h-full min-h-full w-full items-center justify-center mt-16 mb-16">
-      <div className="relative xl:w-[70rem] lg:w-[60rem] h-[653px] items-center justify-center gap-0 rounded-lg bg-white px-0 md:px-20 md:shadow-2xl lg:gap-16">
-        <div className="flex flex-1 items-center justify-center">
-          <img className="absolute xl:scale-x-100 lg:scale-x-90 xl:-left-3 lg:-left-9 lg:top-0" src="/loginwave.svg" alt="Icon"/>
-          <div className="absolute lg:top-24 lg:right-24 flex flex-col justify-center items-center">
-            <h3 className="mb-3 text-slate-900 text-3xl font-medium">Sign In</h3>
-            <h5 className="text-lg font-slate-500 mb-10">Let's build something great</h5>
-            <div className="flex flex-col">
+    <div className="flex h-full w-full items-center justify-center md:mt-16 mb-16">
+      <div className="flex flex-col items-center justify-center lg:relative xl:w-[70rem] lg:w-[60rem] h-[653px] rounded-lg bg-white lg:shadow-xl">
+          <img className="hidden lg:block lg:absolute xl:scale-x-100 lg:scale-x-90 xl:-left-3 lg:-left-9 lg:top-0" src="/loginwave.svg" alt="Icon"/>
+          <div className="hidden lg:block lg:absolute lg:left-16 lg:top-36">
+            <div className="font-bold text-5xl text-white mb-7">
+              Welcome to <br/>Amang
+            </div>
+            <div className="text-white">
+              Please log in to get started<br/>Log in and join the rhythm
+            </div>
+          </div>
+          <div className="lg:absolute lg:top-[8.5rem] lg:right-20 xl:right-32 flex flex-col justify-center items-center">
+            <h3 className={cn(Poppin.className, 'mb-1 text-slate-900 text-2xl font-black')}>Sign In</h3>
+            <h5 className={cn(Poppin.className, 'text-base text-slate-500 mb-8')}>Let's build something great</h5>
             {/* 일반 로그인 */}
-              <form onSubmit={handleSubmit(onValid)} className="space-y-6">
+            <form className="flex flex-col items-center" onSubmit={handleSubmit(onValid)}>
                 <div className="grid w-full max-w-sm items-center gap-1.5">
-                  <Label htmlFor="email">ID</Label>
+                  <Label htmlFor="email" className="font-semibold">ID</Label>
                   <Input    
                     {...register("email")}
                     name="email"
-                    placeholder="Email"
-                    className="text-gray50 h-14 border-slate-300 bg-white px-7 text-xl lg:w-80"/>
+                    placeholder="Input text"
+                    className=" h-12 border-slate-300 shadow-sm bg-white px-7 text-xl lg:w-80"/>
                 </div>             
-                <div className="text-destructive">{errors.email?.message}</div>
-                <div className="grid w-full max-w-sm items-center gap-1.5"></div>
-                  <Label htmlFor="password">PassWord</Label>
+                <div className="text-destructive mb-6">{errors.email?.message}</div>
+                <div className="grid w-full max-w-sm items-center gap-1.5">
+                  <Label htmlFor="password" className="font-semibold">PassWord</Label>
                   <Input
                     {...register("password")}
                     name="password"
-                    placeholder="PW"
+                    placeholder="Input text"
                     type="password"
-                    className="text-gray50 h-14 border-slate-300 bg-white px-7 text-xl lg:w-80"/>
+                    className=" h-12 border-slate-300 shadow-sm bg-white px-7 text-xl lg:w-80 mt-1"/>
+                </div>
                 <div className="text-destructive">{errors.password?.message}</div>
                 <Button
                   type="submit"
-                  className="h-14 w-full bg-blue-500 text-2xl font-extrabold"
+                  className="h-12 w-72 bg-blue-500 text-base font-semibold mt-8"
                   disabled={isSubmitting}
                 >
                   {isSubmitting ? "Login on Progress..." : "Login"}
                 </Button>
-              </form>
-            </div>
+                <div className={cn(Poppin.className, "flex justify-center pt-5")}>
+                  <div className="pr-2">
+                  Don't have an account? 
+                  </div>
+                  <Link href={ROUTES.SIGNUP.url} className={cn(Poppin.className, "text-blue-400")}>
+                    Sign up
+                  </Link>
+                </div>
+                <div className={cn(Poppin.className, "text-blue-400 pt-6")}>Forgot Password?</div>
+            </form>
           </div>
-        </div>
       </div>
     </div>
   )
