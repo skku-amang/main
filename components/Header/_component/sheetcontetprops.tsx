9d2366f66f1f6c3cfef5fb3e902f127822a88fc9
youngjju git <2000dudwn@naver.com>
2024-09-30 18:02:41 +0900

    chore: sheet icons change

--- components/Header/_component/sheetcontetprops.tsx
+++ components/Header/_component/sheetcontetprops.tsx
@@ -1,9 +1,13 @@
+import { FileText,ImageIcon,LogIn,Megaphone, Music4} from 'lucide-react';
 import Link from "next/link"
-import { FaCalendar, FaDoorOpen, FaGuitar, FaInstagram, FaList, FaPeopleArrows, FaYoutube } from "react-icons/fa"
+import { FaInstagram } from "react-icons/fa"
+import { RiYoutubeLine } from 'react-icons/ri';
 
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
 import { Separator } from "@/components/ui/separator"
 import ROUTES from "@/constants/routes"
+
+const iconcolor = "text-gray-500"
 
 const SheetContentProps = () => {
 
@@ -23,19 +27,19 @@ const SheetContentProps = () => {
         <div className="w-full h-[40%] pt-[2%]">
             <div className="w-full h-[12%] text-gray-500 text-base pb-[8%]">MAIN</div>
             <Link href={ROUTES.NOTICE.LIST.url} className='flex items-center font-medium text-lg text-gray-500 w-full h-[22%]'>
-                <FaCalendar size={25} style={{ color: '#BEBEBE' }}/>
+                <Megaphone size={30} className={iconcolor}/>
                 <div className="text-gray-500 font-medium text-lg pl-4">공지사항</div>
             </Link>
             <Link href={ROUTES.PERFORMANCE.LIST.url} className='flex items-center font-medium text-lg text-gray-500 w-full h-[22%]'>
-                <FaList size={25} style={{ color: '#BEBEBE' }} />
+                <Music4 size={30} className={iconcolor}/>
                 <div className="text-gray-500 font-medium text-lg pl-4">공연목록</div>
             </Link>
             <Link href={ROUTES.TEAM.LIST.url} className='flex items-center font-medium text-lg text-gray-500 w-full h-[22%]'>
-                <FaGuitar size={25} style={{ color: '#BEBEBE' }}/>
+                <FileText size={30} className={iconcolor}/>
                 <div className="text-gray-500 font-medium text-lg pl-4">세션지원</div>
             </Link>
             <Link href={ROUTES.MEMBER.LIST.url} className='flex items-center font-medium text-lg text-gray-500 w-full h-[22%]'>
-                <FaPeopleArrows size={25} style={{ color: '#BEBEBE' }}/>
+                <ImageIcon size={30}  className={iconcolor}/>
                 <div className="text-gray-500 font-medium text-lg pl-4">멤버목록</div>                
             </Link>
         </div>
@@ -43,16 +47,16 @@ const SheetContentProps = () => {
         <div className="flex flex-col justify-start w-full h-[24%] pt-[2%]">
             <div className="w-full h-[21.6%] text-gray-500 text-base pb-[13.3%]">LINKS</div>
             <div className='flex items-center text-gray-500 w-full h-[41.4%]'>
-                <FaYoutube size={30} style={{ color: '#BEBEBE' }} />
+                <RiYoutubeLine size={30} className={iconcolor} />
                 <div className='text-gray-500 font-medium text-lg pl-4'>YouTube</div>
             </div>
             <div className='flex items-center text-gray-500 w-full h-[41.4%]'>
-                <FaInstagram size={30} style={{ color: '#BEBEBE' }} />
+                <FaInstagram size={30} className={iconcolor} />
                 <div className='text-gray-500 font-medium text-lg pl-4'>Instagram</div>
             </div>
         </div>
         <Link href={ROUTES.LOGIN.url} className="flex justify-center items-center gap-4 w-full h-[9%] pt-[85%]">
-            <FaDoorOpen size={30} style={{ color: '#023664' }} />
+            <LogIn size={30} className={iconcolor} />
             <div className="text-xl font-medium text-primary">Login Account</div>
         </Link>
     </div>
