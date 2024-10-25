4f07e7cb33a54d40cb30f57aa212b0a14351de50
GalaxyBook2 <manamana32321@gmail.com>
2024-10-26 06:09:02 +0900

    fix: Downgrade Next from 15 to 14 to solve build error

--- env.d.ts
+++ env.d.ts
@@ -1,6 +1,6 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    NODE_ENV: 'development' | 'production'
+    NODE_ENV: 'development' | 'production' | 'test'
     AUTH_SECRET: string
     NEXT_PUBLIC_CLIENT_URL?: string
     NEXT_PUBLIC_SERVER_URL?: string
