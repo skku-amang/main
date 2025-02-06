44acb9533f61e782d57980e8d97fbded22446146
Youngjju <2000dudwn@naver.com>
2025-02-06 23:32:45 +0900

    chore: reservation scaffording

---
+++ app/(general)/reservation/clubroom/page.tsx
@@ -0,0 +1,32 @@
+import { ChevronRight, HouseIcon } from "lucide-react"
+import Link from "next/link"
+
+const ReservationPage = () => {
+  return (
+    <div className="flex h-full w-full flex-col justify-center">
+      <div className="flex flex-col items-center ">
+        <div className="mb-[30px] mt-[95px] flex h-[57px] w-[500px] items-center justify-center text-5xl font-semibold text-black">
+          동아리방 예약
+        </div>
+        <div className="mb-[74px] flex h-[20px] w-[200px] items-center justify-center gap-1">
+          <Link href="/">
+            <HouseIcon className="h-[20px] w-[20px] text-slate-400" />
+          </Link>
+          <ChevronRight className="h-[20px] w-[20px] text-slate-400" />
+          <div className="text-sm font-medium leading-tight text-slate-400">
+            예약
+          </div>
+          <ChevronRight className="h-[20px] w-[20px] text-slate-400" />
+          <div className="text-sm font-medium leading-tight text-primary">
+            동아리방 예약
+          </div>
+        </div>
+      </div>
+      <div className="flex h-[739px] w-full justify-center bg-neutral-300">
+        이곳에 작업하면 됩니다.
+      </div>
+    </div>
+  )
+}
+
+export default ReservationPage
