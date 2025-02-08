d31534f1ce865653124f9e4e166ecd7d796ab052
Youngjju <2000dudwn@naver.com>
2025-02-08 20:46:22 +0900

    chore: delete hardcoding things

--- app/(general)/reservations/clubroom/page.tsx
+++ app/(general)/reservations/clubroom/page.tsx
@@ -5,24 +5,24 @@ const ReservationPage = () => {
   return (
     <div className="flex h-full w-full flex-col justify-center">
       <div className="flex flex-col items-center ">
-        <div className="mb-[30px] mt-[95px] flex h-[57px] w-[500px] items-center justify-center text-5xl font-semibold text-black">
-          물품 대여
+        <div className="mb-8 mt-24 flex h-14 w-full items-center justify-center text-5xl font-semibold text-black">
+          동아리방 예약
         </div>
-        <div className="mb-[74px] flex h-[20px] w-[200px] items-center justify-center gap-1">
+        <div className="mb-20 flex h-5 w-full items-center justify-center gap-1">
           <Link href="/">
-            <HouseIcon className="h-[20px] w-[20px] text-slate-400" />
+            <HouseIcon className="h-5 w-5 text-slate-400" />
           </Link>
-          <ChevronRight className="h-[20px] w-[20px] text-slate-400" />
+          <ChevronRight className="h-5 w-5 text-slate-400" />
           <div className="text-sm font-medium leading-tight text-slate-400">
             예약
           </div>
-          <ChevronRight className="h-[20px] w-[20px] text-slate-400" />
+          <ChevronRight className="h-5 w-5 text-slate-400" />
           <div className="text-sm font-medium leading-tight text-primary">
-            물품 대여
+            동아리방 예약
           </div>
         </div>
       </div>
-      <div className="flex h-[739px] w-full justify-center bg-neutral-500">
+      <div className="flex h-full w-full justify-center bg-neutral-300">
         이곳에 작업하면 됩니다.
       </div>
     </div>
