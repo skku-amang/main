95f2a9462d2bb8cc87571b7f158b9e5cf2150a51
GalaxyBook2 <manamana32321@gmail.com>
2025-03-11 18:32:47 +0900

    refactor: add default page header to performance-related pages

--- app/(general)/(light)/performances/create/page.tsx
+++ app/(general)/(light)/performances/create/page.tsx
@@ -1,8 +1,22 @@
-import PerformanceForm from '../_components/PerformanceForm'
+import { Home } from "lucide-react"
+
+import DefaultPageHeader from "@/components/PageHeaders/Default"
+import ROUTES from "@/constants/routes"
+
+import PerformanceForm from "../_components/PerformanceForm"
 
 const PerformanceCreate = () => {
   return (
     <div>
+      <DefaultPageHeader
+        title="공연 생성"
+        routes={[
+          { display: <Home size={20} strokeWidth={1.67} />, href: ROUTES.HOME },
+          { display: "아카이브" },
+          { display: "공연 생성", href: ROUTES.PERFORMANCE.LIST }
+        ]}
+      />
+
       <PerformanceForm />
     </div>
   )
--- app/(general)/(light)/performances/page.tsx
+++ app/(general)/(light)/performances/page.tsx
@@ -1,11 +1,75 @@
-import PerformanceCardList from './_components/PerformanceCardList'
+import { Home } from "lucide-react"
+import Link from "next/link"
+import { CiCirclePlus } from "react-icons/ci"
 
-const PerformanceList = () => {
+import PerformanceCard from "@/app/(general)/(light)/performances/_components/PerformanceCard"
+import Loading from "@/app/_(errors)/Loading"
+import { auth } from "@/auth"
+import DefaultPageHeader from "@/components/PageHeaders/Default"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import API_ENDPOINTS from "@/constants/apiEndpoints"
+import ROUTES from "@/constants/routes"
+import fetchData from "@/lib/fetch"
+import { ListResponse } from "@/lib/fetch/responseBodyInterfaces"
+import { Performance } from "@/types/Performance"
+
+const PerformanceList = async () => {
+  const session = await auth()
+  if (!session) {
+    return <Loading />
+  }
+  const res = await fetchData(API_ENDPOINTS.PERFORMANCE.LIST, {
+    cache: "no-cache",
+    credentials: "include",
+    headers: {
+      Authorization: `Bearer ${session.access}`
+    }
+  })
+  const performances = (await res.json()) as ListResponse<Performance>
+
   return (
-    <div className="pt-3">
-      <PerformanceCardList />
+    <div>
+      <DefaultPageHeader
+        title="공연 목록"
+        routes={[
+          { display: <Home size={20} strokeWidth={1.67} />, href: ROUTES.HOME },
+          { display: "아카이브" },
+          { display: "공연 목록", href: ROUTES.PERFORMANCE.LIST }
+        ]}
+      />
+
+      {/* 도구 모음 */}
+      <div className="mb-3 flex justify-between">
+        <div className="flex gap-x-3">
+          <Input className="w-72" />
+          <Button>검색</Button>
+        </div>
+        <Button asChild className="flex items-center">
+          <Link href={ROUTES.PERFORMANCE.CREATE}>
+            <CiCirclePlus size={20} />
+            &nbsp;추가
+          </Link>
+        </Button>
+      </div>
+
+      {/* 공연 카드 목록 */}
+      <div className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {performances.map((p) => (
+          <div key={p.id} className="flex w-full justify-center">
+            <PerformanceCard
+              id={p.id}
+              name={p.name}
+              representativeSrc={p.representativeImage}
+              location={p.location}
+              startDatetime={
+                p.startDatetime ? new Date(p.startDatetime) : undefined
+              }
+            />
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
-
 export default PerformanceList
