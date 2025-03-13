881633f874311930abce112e708299895977523a
Youngjju <2000dudwn@naver.com>
2025-03-14 02:11:22 +0900

    trivial

--- app/(general)/(light)/reservations/_components/Calender/AddScheduleButton.tsx
+++ app/(general)/(light)/reservations/_components/Calender/AddScheduleButton.tsx
@@ -22,7 +22,7 @@ export function AddScheduleButton({ className }: { className?: string }) {
       <DialogContent className="sm:max-w-[425px]">
         <DialogTitle>Add Schedule</DialogTitle>
         <div className="text-Zinc-500 h-5 justify-start self-stretch  text-sm font-normal leading-tight">
-          예약 정보를 입력해주세요
+          예약 정보를 입력해주세요.
         </div>
       </DialogContent>
     </Dialog>
