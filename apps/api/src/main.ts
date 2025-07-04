a04d83b59aa0f4871bf565d7d01b4b669f688488
seungmin Nam <nhjbest22@g.skku.edu>
2025-07-04 21:38:44 +0900

    feat(main): add global validation pipe configuration

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   const configService = app.get(ConfigService);
   await app.listen(configService.get<number>('PORT') ?? 8000);
 }
