eb3a36b4c6842fffb24b8bfa58c3915c3dc5dc75
GalaxyBook2 <manamana32321@gmail.com>
2025-07-07 20:34:42 +0900

    refactor: separate providers

---
+++ apps/web/lib/providers/index.tsx
@@ -0,0 +1,14 @@
+"use client"
+import { SessionProvider } from "next-auth/react"
+import { ApiClientProvider } from "./api-client-provider"
+import ReactQueryProvider from "./react-query-provider"
+
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider>
+      <ApiClientProvider>
+        <ReactQueryProvider>{children}</ReactQueryProvider>
+      </ApiClientProvider>
+    </SessionProvider>
+  )
+}
