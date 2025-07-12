886788e2f93245b41aa820ccc902d06e63d8d209
seungmin Nam <nhjbest22@g.skku.edu>
2025-07-13 03:47:02 +0900

    feat(app): import GenerationModule to the main application module

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config';
 import { PrismaModule } from './prisma/prisma.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
+import { GenerationModule } from './generation/generation.module';
 
 @Module({
   imports: [
@@ -15,6 +16,7 @@ import { AuthModule } from './auth/auth.module';
     PrismaModule,
     UsersModule,
     AuthModule,
+    GenerationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
