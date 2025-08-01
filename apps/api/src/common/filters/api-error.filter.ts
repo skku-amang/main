897b5b6426b5649a6d705e5e67723e4538fb3328
seungmin Nam <nhjbest22@g.skku.edu>
2025-08-01 19:42:30 +0900

    feat: add ApiErrorFilter to handle ApiError exceptions and format response

---
+++ apps/api/src/common/filters/api-error.filter.ts
@@ -0,0 +1,29 @@
+import { ExceptionFilter, Catch, ArgumentsHost } from "@nestjs/common"
+import { HttpAdapterHost } from "@nestjs/core"
+import { ApiError, ApiResult, Failure } from "@repo/api-client"
+
+@Catch(ApiError)
+export class ApiErrorFilter implements ExceptionFilter {
+  constructor(private readonly httpAdapterHost: HttpAdapterHost) {}
+
+  catch(exception: ApiError, host: ArgumentsHost): void {
+    console.error("API Error:", exception)
+    const { httpAdapter } = this.httpAdapterHost
+    const ctx = host.switchToHttp()
+    const request = ctx.getRequest<Request>()
+
+    const responseBody = {
+      isSuccess: false,
+      isFailure: true,
+      error: {
+        type: exception.type,
+        status: exception.status,
+        title: exception.title,
+        detail: exception.detail,
+        instance: request.url
+      }
+    } satisfies Failure
+
+    httpAdapter.reply(ctx.getResponse(), responseBody, exception.status)
+  }
+}
