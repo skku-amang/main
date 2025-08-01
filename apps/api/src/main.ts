16efd59ba78aa837eb7306631272497726811929
seungmin Nam <nhjbest22@g.skku.edu>
2025-08-01 19:43:00 +0900

    feat: configure global error handling and response formatting with ApiErrorFilter and ApiResultInterceptor

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,13 +1,18 @@
 import { ConfigService } from "@nestjs/config"
-import { NestFactory } from "@nestjs/core"
+import { HttpAdapterHost, NestFactory } from "@nestjs/core"
 import cookieParser from "cookie-parser"
 import { ZodValidationPipe } from "nestjs-zod"
 import { AppModule } from "./app.module"
+import { ApiErrorFilter } from "./common/filters/api-error.filter"
+import { ApiResultInterceptor } from "./common/interceptors/api-result.interceptor"
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  const httpAdapterHost = app.get(HttpAdapterHost)
   app.use(cookieParser())
   app.useGlobalPipes(new ZodValidationPipe())
+  app.useGlobalFilters(new ApiErrorFilter(httpAdapterHost))
+  app.useGlobalInterceptors(new ApiResultInterceptor())
   const configService = app.get(ConfigService)
   await app.listen(configService.get<number>("PORT") ?? 8000)
 }
