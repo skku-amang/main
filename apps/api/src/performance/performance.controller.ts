4571a8f885f211d24d3460837f67d733efddf51a
seungmin Nam <nhjbest22@g.skku.edu>
2025-09-22 15:45:01 +0900

    refactor: update PerformanceController to enhance route handling and guard usage

--- apps/api/src/performance/performance.controller.ts
+++ apps/api/src/performance/performance.controller.ts
@@ -5,41 +5,58 @@ import {
   Body,
   Patch,
   Param,
-  Delete
+  Delete,
+  UseGuards,
+  ParseIntPipe
 } from "@nestjs/common"
 import { PerformanceService } from "./performance.service"
 import { CreatePerformanceDto } from "./dto/create-performance.dto"
 import { UpdatePerformanceDto } from "./dto/update-performance.dto"
+import { AccessTokenGuard } from "../auth/guards/access-token.guard"
+import { AdminGuard } from "../auth/guards/admin.guard"
+import { Public } from "../auth/decorators/public.decorator"
 
-@Controller("performance")
+@Controller("performances")
+@UseGuards(AccessTokenGuard)
 export class PerformanceController {
   constructor(private readonly performanceService: PerformanceService) {}
 
   @Post()
+  @UseGuards(AdminGuard)
   create(@Body() createPerformanceDto: CreatePerformanceDto) {
     return this.performanceService.create(createPerformanceDto)
   }
 
   @Get()
+  @Public()
   findAll() {
     return this.performanceService.findAll()
   }
 
+  @Get(":id/teams")
+  @Public()
+  findTeamsByPerformanceId(@Param("id", ParseIntPipe) id: number) {
+    return this.performanceService.findTeamsByPerformanceId(id)
+  }
+
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.performanceService.findOne(+id)
+  @Public()
+  findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.performanceService.findOne(id)
   }
 
   @Patch(":id")
+  @UseGuards(AdminGuard)
   update(
-    @Param("id") id: string,
+    @Param("id", ParseIntPipe) id: number,
     @Body() updatePerformanceDto: UpdatePerformanceDto
   ) {
-    return this.performanceService.update(+id, updatePerformanceDto)
+    return this.performanceService.update(id, updatePerformanceDto)
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.performanceService.remove(+id)
+  @UseGuards(AdminGuard)
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.performanceService.remove(id)
   }
 }
