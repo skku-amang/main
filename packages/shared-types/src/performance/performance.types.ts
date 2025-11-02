aaba9a9c1b1860612e364de2625193ff73d6d092
seungmin Nam <nhjbest22@g.skku.edu>
2025-11-02 19:32:30 +0900

    feat: add performance types and include configurations for team and user data

---
+++ packages/shared-types/src/performance/performance.types.ts
@@ -0,0 +1,46 @@
+import { Prisma, Performance } from "@repo/database"
+import { publicUserSelector } from "../user/user.selector"
+
+export type { Performance }
+
+export const performanceFindOneInclude = {
+  teams: {
+    include: {
+      teamSessions: true,
+      leader: {
+        select: publicUserSelector
+      }
+    }
+  }
+} satisfies Prisma.PerformanceInclude
+
+export type PerformanceWithDetails = Prisma.PerformanceGetPayload<{
+  include: typeof performanceFindOneInclude
+}>
+
+export const performanceTeamsInclude = {
+  teams: {
+    include: {
+      teamSessions: {
+        include: {
+          session: true,
+          members: {
+            include: {
+              user: {
+                select: publicUserSelector
+              }
+            },
+            orderBy: { index: "asc" }
+          }
+        }
+      },
+      leader: { select: publicUserSelector }
+    }
+  }
+} satisfies Prisma.PerformanceInclude
+
+export type PerformanceWithTeams = Prisma.PerformanceGetPayload<{
+  include: typeof performanceTeamsInclude
+}>
+
+export type PerformanceTeamsList = PerformanceWithTeams["teams"]
