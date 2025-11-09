65eb907690a000f4bb8bc7a6b8c33a4cc926e3d7
GalaxyBook2 <manamana32321@gmail.com>
2025-11-09 18:22:08 +0900

    refactor: add JSDoc comment for AuthResponse type

--- packages/shared-types/src/auth/auth.types.ts
+++ packages/shared-types/src/auth/auth.types.ts
@@ -2,6 +2,9 @@ import { User } from "@repo/database"
 
 type AuthUser = Omit<User, "password" | "hashedRefreshToken">
 
+/**
+ * 로그인 및 회원가입 응답 타입
+ */
 export type AuthResponse = {
   accessToken: string
   //Refresh Token은 쿠키를 통해 전달됩니다.
