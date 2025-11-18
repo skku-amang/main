e5763d8542db5e43f786e3efe2b6e0e169f4ce29
seungmin Nam <nhjbest22@g.skku.edu>
2025-11-18 21:59:16 +0900

    feat: add unit tests for EquipmentController and EquipmentService

---
+++ apps/api/src/equipment/equipment.controller.spec.ts
@@ -0,0 +1,31 @@
+import { Test, TestingModule } from "@nestjs/testing"
+import { EquipmentController } from "./equipment.controller"
+import { EquipmentService } from "./equipment.service"
+
+describe("EquipmentController", () => {
+  let controller: EquipmentController
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [EquipmentController],
+      providers: [
+        {
+          provide: EquipmentService,
+          useValue: {
+            create: jest.fn(),
+            findMany: jest.fn(),
+            findUnique: jest.fn(),
+            update: jest.fn(),
+            delete: jest.fn()
+          }
+        }
+      ]
+    }).compile()
+
+    controller = module.get<EquipmentController>(EquipmentController)
+  })
+
+  it("should be defined", () => {
+    expect(controller).toBeDefined()
+  })
+})
