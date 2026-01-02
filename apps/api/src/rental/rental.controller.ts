93410e493f38b24bd50cca10e0bd950ee55c7eac
seungmin Nam <nhjbest22@g.skku.edu>
2026-01-02 15:08:59 +0900

    feat: enhance RentalController with guards and improved parameter handling

--- apps/api/src/rental/rental.controller.ts
+++ apps/api/src/rental/rental.controller.ts
@@ -5,38 +5,48 @@ import {
   Body,
   Patch,
   Param,
-  Delete
+  Delete,
+  UseGuards,
+  ParseIntPipe
 } from "@nestjs/common"
 import { RentalService } from "./rental.service"
 import { CreateRentalDto } from "./dto/create-rental.dto"
 import { UpdateRentalDto } from "./dto/update-rental.dto"
+import { AccessTokenGuard } from "../auth/guards/access-token.guard"
+import { Public } from "../auth/decorators/public.decorator"
 
 @Controller("rentals")
+@UseGuards(AccessTokenGuard)
 export class RentalController {
   constructor(private readonly rentalService: RentalService) {}
 
   @Post()
-  create(@Body() createRentalDto: CreateRentalDto) {
+  async create(createRentalDto: CreateRentalDto) {
     return this.rentalService.create(createRentalDto)
   }
 
   @Get()
+  @Public()
   findAll() {
     return this.rentalService.findAll()
   }
 
   @Get(":id")
-  findOne(@Param("id") id: string) {
-    return this.rentalService.findOne(+id)
+  @Public()
+  findOne(@Param("id", ParseIntPipe) id: number) {
+    return this.rentalService.findOne(id)
   }
 
   @Patch(":id")
-  update(@Param("id") id: string, @Body() updateRentalDto: UpdateRentalDto) {
-    return this.rentalService.update(+id, updateRentalDto)
+  update(
+    @Param("id", ParseIntPipe) id: number,
+    @Body() updateRentalDto: UpdateRentalDto
+  ) {
+    return this.rentalService.update(id, updateRentalDto)
   }
 
   @Delete(":id")
-  remove(@Param("id") id: string) {
-    return this.rentalService.remove(+id)
+  remove(@Param("id", ParseIntPipe) id: number) {
+    return this.rentalService.remove(id)
   }
 }
