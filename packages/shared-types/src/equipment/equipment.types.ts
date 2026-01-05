ef57c1614dfe686a11693c15f2d910323ccd3349
seungmin Nam <nhjbest22@g.skku.edu>
2026-01-05 21:34:32 +0900

    fix: remove misdeclared types

--- packages/shared-types/src/equipment/equipment.types.ts
+++ packages/shared-types/src/equipment/equipment.types.ts
@@ -6,9 +6,6 @@ export const equipmentWithRentalLogInclude = {
   rentalLogs: true
 } satisfies Prisma.EquipmentInclude
 
-type EquipmentWithRentalLog = Prisma.EquipmentGetPayload<{
+export type EquipmentWithRentalLog = Prisma.EquipmentGetPayload<{
   include: typeof equipmentWithRentalLogInclude
 }>
-
-export type RentalList = EquipmentWithRentalLog[]
-export type RentalDetail = EquipmentWithRentalLog
