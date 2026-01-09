6617442e56fb73ad90ca52c77588f1f295863833
seungmin Nam <nhjbest22@g.skku.edu>
2026-01-09 18:55:30 +0900

    fix: remove unused Public decorator from findAll method to ensure API used by logged in user

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,6 +1,5 @@
 import { Controller, Get, UseGuards } from "@nestjs/common"
 import { AccessTokenGuard } from "../auth/guards/access-token.guard"
-import { Public } from "../auth/decorators/public.decorator"
 import { UsersService } from "./users.service"
 
 @Controller("users")
@@ -9,7 +8,6 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
-  @Public()
   findAll() {
     return this.userService.findAll()
   }
