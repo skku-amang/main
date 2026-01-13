1fc69caf5c2512140618e1d0b80693b8f57584e0
seungmin Nam <nhjbest22@g.skku.edu>
2026-01-13 16:29:12 +0900

    feat: update performance seeding logic with multiple events

--- packages/database/prisma/seeds/04-performance.seed.ts
+++ packages/database/prisma/seeds/04-performance.seed.ts
@@ -1,20 +1,54 @@
 import { PrismaClient } from "../../generated/prisma"
+import { getDate } from "./utils"
 
 export const seedPerformance = async (prisma: PrismaClient) => {
   console.log("Seeding performance...")
 
-  await prisma.performance.upsert({
-    where: { id: 1 },
-    update: {},
-    create: {
-      name: "샘플 공연",
-      description: "샘플 공연 설명입니다.",
+  const performaces = [
+    {
+      name: "2025 새내기 배움터 공연",
+      description: "2025 새내기 배움터 공연입니다.",
       posterImage: "https://picsum.photos/200/300",
-      location: "동방",
-      startAt: new Date("2025-09-28T18:00:00Z"),
-      endAt: new Date("2025-09-28T21:00:00Z")
+      location: "MT 대강당",
+      startAt: getDate(-365),
+      endAt: getDate(-305, 21)
+    },
+    {
+      name: "2025 가을 정기 공연",
+      description: "2025 가을 정기 공연입니다.",
+      posterImage: "https://picsum.photos/200/300",
+      location: "홍대 롤러코스터 라이브홀",
+      startAt: getDate(-120),
+      endAt: getDate(-60, 21)
+    },
+    {
+      name: "2026 새내기 배움터 공연",
+      description: "2026 새내기 배움터 공연입니다.",
+      posterImage: "https://picsum.photos/200/300",
+      location: "MT 대강당",
+      startAt: getDate(-30),
+      endAt: getDate(30, 21)
+    },
+    {
+      name: "2026 1학기 정기 공연",
+      description: "2026 1학기 정기 공연입니다.",
+      posterImage: "https://picsum.photos/200/300",
+      location: "홍대 롤러코스터 라이브홀",
+      startAt: getDate(60),
+      endAt: getDate(120, 21)
+    },
+    {
+      name: "2026 2학기 정기 공연",
+      description: "2026 2학기 정기 공연입니다.",
+      posterImage: "https://picsum.photos/200/300",
+      location: "홍대 롤러코스터 라이브홀",
+      startAt: getDate(180),
+      endAt: getDate(240, 21)
     }
-  })
+  ]
 
+  await prisma.performance.createMany({
+    data: performaces
+  })
   console.log("Seeding performance completed.")
 }
