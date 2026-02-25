594ccad4e641be33a37a86c8ed8d9e1ee8c3e9bb
manamana32321 <manamana32321@users.noreply.github.com>
2026-02-25 08:57:48 +0000

    Deploying to gh-pages from @ skku-amang/main@a913d4790447704531ee11f25addefa0fcd4d8c3 🚀

---
+++ pr-308/assets/Search.stories-BLJ3f3sF.js
@@ -0,0 +1,12 @@
+import{j as e,c as d}from"./utils-De1j6o0y.js";import{R as n}from"./iframe-ByFymh9P.js";import{c as l}from"./createLucideIcon-D9YyGpgU.js";import"./preload-helper-PPVm8Dsz.js";const p=l("Search",[["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]),t=n.forwardRef(({className:o,...c},i)=>e.jsxs("div",{className:d("flex h-10 w-80 items-center gap-x-2.5 rounded-full border border-gray-200 bg-white px-[13px] py-2 text-sm ring-offset-background drop-shadow-[0_1px_2px_rgb(64,63,84,0.1)] transition duration-300 focus-within:ring-0 focus-within:ring-offset-0 focus-within:drop-shadow-[0_4px_4px_rgba(0,0,0,0.25)]",o),children:[e.jsx(p,{size:24,className:"text-gray-500",strokeWidth:1.25}),e.jsx("input",{...c,placeholder:"검색",type:"search",ref:i,className:"w-full placeholder:text-gray-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50",style:{caretColor:"#111827"}})]}));t.displayName="Search";t.__docgenInfo={description:"",methods:[],displayName:"Search"};const f={title:"Search",component:t,tags:["autodocs"]},r={},s={args:{className:"w-60"}},a={args:{className:"w-full"},decorators:[o=>e.jsx("div",{className:"w-[400px]",children:e.jsx(o,{})})]};r.parameters={...r.parameters,docs:{...r.parameters?.docs,source:{originalSource:"{}",...r.parameters?.docs?.source}}};s.parameters={...s.parameters,docs:{...s.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: "w-60"
+  }
+}`,...s.parameters?.docs?.source}}};a.parameters={...a.parameters,docs:{...a.parameters?.docs,source:{originalSource:`{
+  args: {
+    className: "w-full"
+  },
+  decorators: [Story => <div className="w-[400px]">
+        <Story />
+      </div>]
+}`,...a.parameters?.docs?.source}}};const g=["Default","CustomWidth","FullWidth"];export{s as CustomWidth,r as Default,a as FullWidth,g as __namedExportsOrder,f as default};
