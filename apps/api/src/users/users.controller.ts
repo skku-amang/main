21590bcdf68ddb79fc4ef61d0bbf9296cd0a4727
seungmin Nam <nhjbest22@g.skku.edu>
2026-03-03 17:40:40 +0900

    feat: add create user endpoint with admin guard

--- apps/api/src/users/users.controller.ts
+++ apps/api/src/users/users.controller.ts
@@ -1,6 +1,9 @@
-import { Controller, Get, UseGuards } from "@nestjs/common"
+import { Controller, Get, Post, Body, UseGuards } from "@nestjs/common"
 import { AccessTokenGuard } from "../auth/guards/access-token.guard"
 import { UsersService } from "./users.service"
+import { Public } from "../auth/decorators/public.decorator"
+import { AdminGuard } from "src/auth/guards/admin.guard"
+import { CreateUserDto } from "../users/dto/create-user.dto"
 
 @Controller("users")
 @UseGuards(AccessTokenGuard)
@@ -8,7 +11,14 @@ export class UsersController {
   constructor(private readonly userService: UsersService) {}
 
   @Get()
+  @Public()
   findAll() {
     return this.userService.findAll()
+  }
+
+  @Post()
+  @UseGuards(AdminGuard)
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto)
   }
 }
